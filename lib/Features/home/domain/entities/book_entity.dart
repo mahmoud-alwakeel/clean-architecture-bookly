5d82ffeb3b57ab472f6c1797fe6ad47b97cb8699
mahmoud-alwakeel <mahmoudalwakeel200@gmail.com>
2024-04-08 00:01:37 +0200

    book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String author;
+  final num price;
+  final num rating;
+
+  BookEntity(this.image, this.title, this.author, this.price, this.rating);
+}
